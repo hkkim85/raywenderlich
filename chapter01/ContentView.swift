c52ce910a2b2eb2614af44613db0194f3a9e316b
hkkim85 <hkkim85@icloud.com>
2022-02-19 12:52:44 +0900

    chapter01

--- chapter01/ContentView.swift
+++ chapter01/ContentView.swift
@@ -9,13 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/)
+            Text(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/)
+            HStack {
+                Text("1")
+                Slider(value: .constant(10), in: 1.0...100.0)
+                Text("100")
+            }
+            Button(action:{}) {
+                Text("Hit me")
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        ContentView().previewLayout(.fixed(width: 568, height: 320))
     }
 }
+
