02d0b715575fdae1f3e6771077091cc1e83382fd
hkkim85 <hkkim85@icloud.com>
2022-02-19 12:38:17 +0900

    Initial Commit

---
+++ chapter01/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  chapter01
+//
+//  Created by 김홍규 on 2022/02/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ chapter01/chapter01App.swift
@@ -0,0 +1,17 @@
+//
+//  chapter01App.swift
+//  chapter01
+//
+//  Created by 김홍규 on 2022/02/19.
+//
+
+import SwiftUI
+
+@main
+struct chapter01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
